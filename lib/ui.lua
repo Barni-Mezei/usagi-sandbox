dbb45ae70efb79688296e08c0bfaa9c3e7233bbc
Barni-Mezei <mezeibg@gmail.com>
2026-05-12 14:33:50 +0200

    Moved the UI experimentations into another project (**usagi-ui**)

--- lib/ui.lua
+++ lib/ui.lua
@@ -2,34 +2,44 @@
 ---@diagnostic disable: return-type-mismatch
 
 ---@class UI
----@field screen_box UI.Box A box covering the whole screen
----@field panels     table  A table containing the top level UI items
+---@field screen_box  UI.Box A box covering the whole screen
+---@field panels      table  A table containing the top level UI items
+---@field value_hooks table  A table that contains all value hooks and their current value
 local M = {
     screen_box = {},
     panels = {},
+    value_hooks = {},
 }
 
 ---@class UI.Box
----@field type     string The type of this element: "box"
----@field x        number The X coordinate of the top-left corner of the box
----@field y        number The Y coordinate of the top-left corner of the box
----@field w        number The width of the box
----@field h        number The height of the box
----@field mx       number The outside margin of the box on the X axis
----@field my       number The outside margin of the box on the Y axis
----@field children table  A list containing child items
+--- This is the basis of ALL other ui items! **Every item has these properties**
+---@field type     string  The type of this element: "box"
+---@field x        number  The X coordinate of the top-left corner of the box
+---@field y        number  The Y coordinate of the top-left corner of the box
+---@field w        number  The width of the box
+---@field h        number  The height of the box
+---@field min_w    number  The minimum allowed width of the box
+---@field min_h    number  The minimum allowed height of the box
+---@field max_w    number  The maximum allowed width of the box
+---@field max_h    number  The maximum allowed height of the box
+---@field mx       number  The outside margin of the box on the X axis
+---@field my       number  The outside margin of the box on the Y axis
+---@field fix_size boolean Toggles whenever to update the size of this item, relative to it's children
+---@field children table   A list containing child items
 
 ---@class UI.Label
 --- Same as UI.Box, but has some additional parameters for text rendering
----@field type    string The type of this element: "label"
----@field text    string  The text of the label
----@field h_align integer The horizontal alignment of the text inside the box (-1: left, 0: center, 1: right )
----@field v_align integer The vertical alignment of the text inside the box   (-1: top,  0: center, 1: bottom)
+---@field type       string The type of this element: "label"
+---@field text       string  The text of the label
+---@field value_hook string  A reference to an entry in the `value_hooks` table
+---@field h_align    integer The horizontal alignment of the text inside the box (-1: left, 0: center, 1: right )
+---@field v_align    integer The vertical alignment of the text inside the box   (-1: top,  0: center, 1: bottom)
 
 ---@class UI.List
 --- Same as UI.Box, but has some additional parameters for aligning child items
----@field type    string The type of this element: "list"
+---@field type    string  The type of this element: "list"
 ---@field axis    string  The axis to align items along (can be "x" or "y")
+---@field gap     number  The gap between the items in this list
 ---@field h_align integer The horizontal alignment of the items inside the box (-1: left, 0: center, 1: right )
 ---@field v_align integer The vertical alignment of the items inside the box   (-1: top,  0: center, 1: bottom)
 
@@ -51,13 +61,22 @@ function M.create_box(x, y, w, h, mx, my)
     local new_box = {
         type = "box",
         children = {},
+        fix_size = false,
     }
+
+    function new_box.add_child(item)
+        table.insert(new_box.children, item)
+    end
 
     if type(x) == "table" then
         new_box.x = x.x or 0
         new_box.y = x.y or 0
         new_box.w = x.w or 0
         new_box.h = x.h or 0
+        new_box.min_w = x.min_w or 0
+        new_box.min_h = x.min_h or 0
+        new_box.max_w = x.max_w or 1000
+        new_box.max_h = x.max_h or 1000
         new_box.mx = x.mx or 0
         new_box.my = x.my or 0
     else
@@ -65,6 +84,10 @@ function M.create_box(x, y, w, h, mx, my)
         new_box.y = y or 0
         new_box.w = w or 0
         new_box.h = h or 0
+        new_box.min_w = 0
+        new_box.min_h = 0
+        new_box.max_w = 1000
+        new_box.max_h = 1000
         new_box.mx = mx or 0
         new_box.my = my or 0
     end
@@ -76,12 +99,13 @@ end
 ---@param text     string|UI.Label The text to create a boundary around or a partially populated table
 ---@param v_align? integer The vertical alignment of the text inside the box   (-1: top,  0: center, 1: bottom)
 ---@param h_align? integer The horizontal alignment of the text inside the box (-1: left, 0: center, 1: right )
+---@param value_hook? string The hook on which you can modify this label's text
 ---@return UI.Label label  A new box with the size of the provided text
-function M.create_label(text, v_align, h_align)
+function M.create_label(text, v_align, h_align, value_hook)
     local new_label = {}
 
     if type(text) == "table" then
-        new_label = M.create_box(text.x, text.y, text.w, text.h, text.mx, text.my)
+        new_label = M.create_box(text)
         new_label.text = text.text or ""
         new_label.v_align = text.v_align or 0
         new_label.h_align = text.h_align or 0
@@ -94,40 +118,74 @@ function M.create_label(text, v_align, h_align)
     end
 
     new_label.type = "label"
+    new_label.value_hook = value_hook or nil
+
+    -- Create hook endpoint for this label
+    if value_hook ~= nil then
+        M.value_hooks[value_hook] = new_label.text
+    end
+
     return new_label
 end
 
 ---Updates a label. Should be called after changing the label's text
----@param label UI.Label  The label to update
+---@param item UI.Label  The label to update
 ---@return UI.Label label The updated label
-function M.update_label(label)
-    local w, h = usagi.measure_text(label.text)
+function M.update_label(item)
+    if item.type ~= "label" then return item end
 
-    label.w = w
-    label.h = h
+    -- Update text from the hook
+    if item.value_hook ~= nil then
+        item.text = tostring( M.value_hooks[item.value_hook] )
+    end
 
-    return label
+    -- Update size
+    if item.fix_size then
+        local w, h = usagi.measure_text(item.text)
+
+        item.w = w
+        item.h = h
+        item.min_w = w
+        item.min_h = h
+    end
+
+    return item
+end
+
+---Updates the value of a hook
+---@param value_hook string The ID of a value hook
+---@param new_value  string The value to write into the hook
+function M.set_hook(value_hook, new_value)
+    M.value_hooks[value_hook] = new_value
+end
+
+---Returns with a value in a value hook
+---@param value_hook string The ID of a value hook
+---@return string|nil value The value stored at the specified value hook
+function M.get_hook(value_hook)
+    return M.value_hooks[value_hook]
 end
 
 ---Creates a new list box
----@param w        number|UI.List The width of the list container or
+---@param axis     string|UI.List The axis to align items along (can be "x" or "y") or
 ---a partially popupated table
----@param h?       number  The height of the list container
----@param axis?    string  The axis to align items along (can be "x" or "y")
+---@param gap?     number  The gap between the items in this list
 ---@param v_align? integer The vertical alignment of the items in the list   (-1: top,  0: center, 1: bottom)
 ---@param h_align? integer The horizontal alignment of the items in the list (-1: left, 0: center, 1: right )
 ---@return UI.List list    A new list container
-function M.create_list(w, h, axis, h_align, v_align)
+function M.create_list(axis, gap, h_align, v_align)
     local new_list = {}
 
-    if type(w) == "table" then
-        new_list = M.create_box(w.x, w.y, w.w, w.h, w.mx, w.my)
-        new_list.axis = w.axis or "x"
-        new_list.v_align = w.v_align or 0
-        new_list.h_align = w.h_align or 0
+    if type(axis) == "table" then
+        new_list = M.create_box(axis)
+        new_list.axis = axis.axis or "x"
+        new_list.gap = axis.gap or 0
+        new_list.v_align = axis.v_align or 0
+        new_list.h_align = axis.h_align or 0
     else
-        new_list = M.create_box(0, 0, w, h)
+        new_list = M.create_box({})
         new_list.axis = axis or "x"
+        new_list.gap = gap or 0
         new_list.v_align = v_align or 0
         new_list.h_align = h_align or 0
     end
@@ -181,7 +239,8 @@ end
 ---Renders a ui item on the screen
 ---@param item        table   The UI item to render (can be a box or a label) 
 ---@param debug_mode? boolean Render box outlines?
-function M.render_item(item, debug_mode)
+local function _render_item(item, debug_mode)
+
     -- Render label text
     if item.text ~= nil then
         local tw, th = usagi.measure_text(item.text)
@@ -195,10 +254,10 @@ function M.render_item(item, debug_mode)
         gfx.text(item.text, tc.x, tc.y, gfx.COLOR_WHITE)
     end
 
-    if debug_mode then
+    if item ~= nil and debug_mode then
         -- Render margin
         gfx.rect(item.x - item.mx, item.y - item.my, item.w + item.mx*2, item.h + item.my*2, gfx.COLOR_LIGHT_GRAY)
-    
+
         -- Render item boundary
         gfx.rect(item.x, item.y, item.w, item.h, gfx.COLOR_RED)
     end
@@ -208,57 +267,197 @@ end
 ---@param original UI.Box|UI.Label|UI.List The original item to merge into
 ---@param new      UI.Box|UI.Label|UI.List The item to merge the data from
 ---@return UI.Box|UI.Label|UI.List item    The merged item 
-function M.merge_item(original, new)
-    local out = original
-
+local function _merge_item(original, new)
     for k, v in pairs(new) do
+        if original[k] == nil then
+            original[k] = v
+        end
+
         if k ~= "children" and k ~= "type" then
-            out[k] = v
+            original[k] = v
         end
     end
 
-    return out
+    return original
 end
 
----Fills in the default values for each item in the provided item and it's children
+---Fills in the default values in the provided item
 ---@param item UI.Box|UI.Label|UI.List  The item to initialise
 ---@return UI.Box|UI.Label|UI.List item The initialised item
 function M.initialise(item)
-    if item.type == "box" then item = M.merge_item(item, M.create_box(item)) end
-    if item.type == "label" then item = M.merge_item(item, M.create_label(item)) end
-    if item.type == "list" then item = M.merge_item(item, M.create_list(item)) end
+    if item.type == "panel" then
+        local p = M.create_box(item)
+        p.type = "panel"
+        item = _merge_item(item, p)
+    end
+    if item.type == "box" then
+        local b = M.create_box(item)
+        item = _merge_item(item, b)
+    end
+    if item.type == "label" then
+        item = _merge_item(item, M.create_label(item))
+        item = M.update_label(item)
+    end
+    if item.type == "list" then item = _merge_item(item, M.create_list(item)) end
 
     -- Iterate over all children
     if item.children ~= nil then
-        for i, n in ipairs(item.children) do
-            item.children[i] = M.initialise(n)
+        for i, child in ipairs(item.children) do
+            item.children[i] = M.initialise(child)
         end
     end
 
     return item
 end
 
-function M.init()
-    -- Create screen box
-    M.screen_box = M.create_box(0, 0, usagi.GAME_W, usagi.GAME_H, 0, 0)
-
-    M.panels = {}
-end
-
-function M.render(debug_mode) end
-function M.update(mouse_x, mouse_y) end
-
 ---Appends a new panel into the UI
----@param item UI.Box|UI.Label|UI.List The insode of the panel
+---@param item UI.Box|UI.Label|UI.List A tree of UI items, which can be partially filled
 ---@param h_align? integer The horizontal alignment of this panel rin the screen (-1: left, 0: center, 1: right )
 ---@param v_align? integer The vertical alignment of this panel rin the screen   (-1: top,  0: center, 1: bottom)
 function M.set_panel(item, h_align, v_align)
-    table.insert(M.panels, {
+    local new_panel = {
         type = "panel",
         h_align = h_align or -1,
         v_align = v_align or -1,
-        item = M.initialise(item),
-    })
+        children = {item},
+    }
+
+    table.insert(M.panels, M.initialise(new_panel))
 end
+
+--
+--  Main methods
+--
+
+function M.init()
+    -- Create screen box
+    M.screen_box = M.create_box(0, 0, usagi.GAME_W, usagi.GAME_H)
+    M.screen_box.mx = 0
+    M.screen_box.my = 0
+    M.screen_box.min_w = usagi.SPRITE_SIZE
+    M.screen_box.min_h = usagi.SPRITE_SIZE
+    M.screen_box.max_w = usagi.GAME_W
+    M.screen_box.max_h = usagi.GAME_H
+
+    -- Panel continer
+    M.panels = {}
+end
+
+local function _render_loop(item, debug_mode)
+    local d = debug_mode or false
+
+    -- Iterate over all children
+    if item.children ~= nil then
+        for _, child in ipairs(item.children) do
+            _render_loop(child, d)
+        end
+    end
+
+    _render_item(item, d)
+end
+
+function M.render(debug_mode)
+    for _, panel in pairs(M.panels) do
+        _render_loop(panel, debug_mode)
+    end
+
+    _render_item(M.screen_box, debug_mode)
+end
+
+local function _size_update_loop(item)
+    -- Iterate over all children and update them
+    for i, child in ipairs(item.children) do
+        item.children[i] = _size_update_loop(child)
+    end
+
+    if item.type == "box" then
+        item.w = math.max(item.min_w, item.w)
+        item.h = math.max(item.min_h, item.h)
+
+        for _, child in pairs(item.children) do
+            item.w = math.min(item.w, child.w + child.mx * 2)
+            item.h = math.min(item.h, child.h + child.my * 2)
+        end
+    end
+
+    if item.type == "label" then
+        item = M.update_label(item)
+    end
+
+    if item.type == "list" then
+        item.w, item.h = 0, 0
+
+        if item.axis == "x" then
+            for _, child in pairs(item.children) do
+                item.w += child.w + child.mx*2 + item.gap
+                item.h = math.max(item.h, child.h)
+            end
+
+            item.w -= item.gap
+        end
+
+        if item.axis == "y" then
+            for _, child in pairs(item.children) do
+                item.w = math.max(item.w, child.w)
+                item.h += child.h + child.my*2 + item.gap
+            end
+
+            item.h -= item.gap
+        end
+    end
+
+    -- Constrain item sizes
+    item.w = math.min(math.max(item.w, item.min_w), item.max_w)
+    item.h = math.min(math.max(item.h, item.min_h), item.max_h)
+
+    return item
+end
+
+local function _position_update_loop(item, parent_x, parent_y)
+    local px = parent_x or 0
+    local py = parent_y or 0
+
+    -- Apply margin offset
+    item.x = px + item.mx
+    item.y = py + item.my
+
+    -- Iterate over all children and update them
+    local dpx = px + item.mx
+    local dpy = py + item.my
+
+    for i, child in ipairs(item.children) do
+        item.children[i] = _position_update_loop(child, dpx, dpy)
+
+        if item.type == "list" then
+            if item.axis == "x" then
+                dpx += child.w + child.mx*2 + item.gap
+            end
+
+            if item.axis == "y" then
+                dpy += child.h + child.my*2 + item.gap
+            end
+        end
+    end
+
+    if item.type == "box" then end
+    if item.type == "label" then end
+
+
+    return item
+end
+
+function M.update(mouse_x, mouse_y)
+    -- Calculate element sizes
+    for _, panel in pairs(M.panels) do
+        _size_update_loop(panel)
+    end
+
+    -- Calculate item positions
+    for _, panel in pairs(M.panels) do
+        _position_update_loop(panel, M.screen_box.x, M.screen_box.y)
+    end
+end
+
+
 
 return M
--- main.lua
+++ main.lua
@@ -2,55 +2,252 @@
 ---@diagnostic disable: return-type-mismatch
 
 local ui = require("lib.ui")
+local grid = require("lib.grid")
+local v = require("lib.vector")
+
 require("lib.misc")
+
+--[[
+TODO:
+
+Updating cells:
+- Generate new state into State.buffer
+- then copyting buffer to the grid
+
+cell updates -> material callback? (slow)
+
+Materials:
+- sand (falls, piles up)
+- water (falls, flows)
+- wall (static, acid can destroy it)
+- acid (it's like water, but has a chance to destroy solids)
+- wood (same as wall, but can be set on fire and is weaker towards acid)
+- fire (spreads to nearby flammable cells, creates smoke)
+- smoke (rises up)
+- methane (like smoke, but is flammable)
+- black hole (static, consumes everything)
+
+Future:
+- chunked updating (quad tree?)
+
+]]
 
 function _config()
 	return {
-		name = "UI test",
-		game_id = "com.barni-07.ui-test",
+		name = "Sandbox",
+		game_id = "com.barni-07.sandbox",
 		icon = 1,
 
-		-- game_width = 640,
-		-- game_height = 360,
+		game_width = 640,
+		game_height = 360,
 	}
 end
 
 -- Mouse position
 local mx, my = 0, 0
+local cx, cy = 0, 0
+
+Settings = {
+	grid_width = 20,
+	grid_height = 20,
+	cell_size = 1,
+}
+
+Settings.cell_size = math.floor(
+	math.min(
+		usagi.GAME_W / Settings.grid_width,
+		usagi.GAME_H / Settings.grid_height
+	)
+)
+
+Materials = {
+	{
+		title = "Air",
+		color = {gfx.COLOR_DARK_BLUE},
+	},
+	{
+		title = "Sand",
+		color = {gfx.COLOR_YELLOW, gfx.COLOR_ORANGE},
+	},
+	{
+		title = "Water",
+		color = {gfx.COLOR_BLUE},
+	},
+	{
+		title = "Concrete",
+		color = {gfx.COLOR_DARK_GRAY, gfx.COLOR_LIGHT_GRAY},
+	},
+	{
+		title = "Wood",
+		color = {gfx.COLOR_BROWN},
+	},
+	{
+		title = "Acid",
+		color = {gfx.COLOR_GREEN},
+	},
+}
+
+local function get_material_index(name)
+	for i, m in ipairs(Materials) do
+		if m.title == name then return i end
+	end
+
+	return -1
+end
+
+-- UI stuff
+
+local screen_box = {}
+local grid_box = {}
+
+local function _init_ui()
+	screen_box = ui.create_box(0, 0, usagi.GAME_W, usagi.GAME_H)
+
+	grid_box = ui.create_box(0, 0, State.grid.width * Settings.cell_size, State.grid.height * Settings.cell_size)
+	grid_box = ui.align_item(grid_box, screen_box, 0, 0)
+end
 
 function _init()
-	ui.init()
+	-- Keep objects between reloads
+	Pool = require("lib.pool")
+	Pool.init()
+	Pool.max_size = -1
 
-	local tree = {
-		type = "list",
-		axis = "y",
-		children = {
-			{
-				type = "label",
-				text = "Line 1",
-			},
-			{
-				type = "label",
-				text = "Line 2",
-			}
-		}
+	input.set_mouse_visible(false)
+
+	Random = require("lib.random")
+
+	State = {
+		-- UI
+		selected_material = 2,
+		is_paused = false,
+		brush_size = 3,
+
+		grid = {},
+		buffer = {},
 	}
 
-	-- Add left panel
-	ui.set_panel(tree, -1, -1)
+	-- Pre generate the grid and the back buffer
+	State.grid   = grid.create_grid(Settings.grid_width, Settings.grid_height, get_material_index("Air"))
+	State.buffer = grid.copy_grid(State.grid)
 
-	dump(ui)
-	os.exit()
+	_init_ui()
+end
+
+local function _update_ui()
+	-- Single button controls
+	if input.key_pressed(input.KEY_SPACE) then State.is_paused = not State.is_paused end
+
+	if input.pressed(input.LEFT)  then State.selected_material = util.clamp(State.selected_material - 1, 2, #Materials) end
+	if input.pressed(input.RIGHT) then State.selected_material = util.clamp(State.selected_material + 1, 2, #Materials) end
+
+	if input.pressed(input.UP)   then State.brush_size = util.clamp(State.brush_size + 1, 1, 5) end
+	if input.pressed(input.DOWN) then State.brush_size = util.clamp(State.brush_size - 1, 1, 5) end
+
+
+	-- Allow cell manipulation if the cursor is over the grid
+	if cx ~= -1 or cy ~= -1 then
+		-- Left click to place
+		if input.mouse_held(input.MOUSE_LEFT) then
+			foreach_brush(
+				grid_box, State.grid, Settings.cell_size,
+				cx, cy, State.brush_size,
+				function (cell_x, cell_y, screen_x, screen_y)
+					grid.set_cell(State.grid, cell_x, cell_y, State.selected_material)
+				end
+			)
+		end
+	
+		-- Right click to erase
+		if input.mouse_held(input.MOUSE_RIGHT) then
+			foreach_brush(
+				grid_box, State.grid, Settings.cell_size,
+				cx, cy, State.brush_size,
+				function (cell_x, cell_y, screen_x, screen_y)
+					grid.set_cell(State.grid, cell_x, cell_y, get_material_index("Air"))
+				end
+			)
+		end
+	end
+
+	return nil
 end
 
 function _update(dt)
 	mx, my = input.mouse()
+	cx, cy = screen_to_cell(grid_box, Settings.cell_size, mx, my)
 
-	ui.update(mx, my)
+	-- Update the UI if the cursor is on screen
+	if util.point_in_rect({x = mx, y = my}, {x = 0, y = 0, w = usagi.GAME_W, h = usagi.GAME_H}) then
+		_update_ui()
+	end
+
+	-- Update cells
+	grid.foreach(State.grid, function (x, y, value)
+		local cell = value
+
+		if cell == get_material_index("Sand") then
+			if grid.get_cell(State.grid, x, y + 1) == get_material_index("Air") then
+				cell = get_material_index("Air")
+			end
+		end
+
+		grid.set_cell(State.buffer, x, y, cell)
+	end)
+
+	-- Copy buffer
+	State.grid = grid.copy_grid(State.buffer)
 end
 
 function _draw(dt)
 	gfx.clear(gfx.COLOR_BLACK)
 
-	ui.render(true)
+	-- Render grid
+	grid.foreach(State.grid, function (x, y, value)
+		local sx, sy = cell_to_screen(grid_box, Settings.cell_size, x, y)
+
+		-- Use the Rng for coordinate hashing
+		local r = Random.create_rng(y*Settings.grid_width*100 + x + value*4)
+
+		gfx.rect_fill(
+			sx, sy,
+			Settings.cell_size, Settings.cell_size,
+			r.rand_item(Materials[value].color)
+		)
+	end)
+
+	-- Render UI
+	local material_label = ui.create_label(f("[ %s ]", Materials[State.selected_material].title))
+	material_label.mx = 4
+	material_label = ui.align_item(material_label, screen_box, 1, -1)
+
+	local brush_label = ui.create_label(f("Brush: %d", State.brush_size))
+	brush_label.mx = 4
+	brush_label = ui.align_item(brush_label, screen_box, 1, -1)
+	brush_label.y += material_label.h
+
+	ui.render_item(material_label)
+	ui.render_item(brush_label)
+
+	-- Pause text
+	if State.is_paused then
+		local pause_label = ui.create_label("PAUSED")
+		pause_label.my += 8
+		pause_label = ui.align_item(pause_label, screen_box, 0, -1)
+
+		ui.render_item(pause_label)
+	end
+
+	-- Render brush preview and cursor
+	if cx ~= -1 or cy ~= -1 then
+		foreach_brush(
+			grid_box, State.grid, Settings.cell_size,
+			cx, cy, State.brush_size,
+			function (cell_x, cell_y, screen_x, screen_y)
+				gfx.rect(screen_x, screen_y, Settings.cell_size, Settings.cell_size, gfx.COLOR_LIGHT_GRAY)
+			end
+		)
+	end
+
+	gfx.spr(2, mx - 8, my - 8)
 end
